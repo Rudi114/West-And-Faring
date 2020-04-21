5142a2cce8a374ec237578c5fd30197e8a9097c1
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2020-04-21 16:11:08 -0700

    began building new contact page

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,15 +1,12 @@
 <template lang="pug">
-  div()
+  div(class="current-page")
     img(src="../images/contact/cp_shadow.png" class="cp_shadow")
     h3(class="contact-title")
       | Contact
-    p(class="contact-about")
-      a(href="mailto:westandfaring1@gmail.com" class="the-styling")
-        | westandfaring1@gmail.com
-      br
-      a(href="tel:925-451-4213" class="the-styling")
-        | (925)451-4213
-    //- img(src="../images/contact/cp_temp.png" class="cp_temp")
+    div(class="contact-form")
+      input(v-model="name" placeholder="name" id="name" :style="{left: this.nameMove}" v-on:keyup.13='onEnter()')
+      input(v-model="email" placeholder="email" id="email" :style="{left: this.emailMove}")
+      textarea(v-model="message" placeholder="message" id="message" :style="{left: this.messageMove}")
 </template>
 
 <script lang="ts">
@@ -22,11 +19,36 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 
 export default class ContactPage extends Vue {
+  public nameMove: string = '14vw';
+  private emailMove: string = '14vw';
+  private messageMove: string = '14vw';
+  public name: string = '';
+  private email: string = ''
+  private message: string = ''
+
+
+  onEnter() {
+    var input = document.getElementById("name");
+    if(input !== null) {
+      input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+          console.log('this far')
+          event.preventDefault();
+          var name = '28vw';
+          return name;
+        }
+      });
+      this.nameMove = name
+      console.log(name)
+    }
+  }
+
+
 }
 </script>
 
 <style lang="scss" scoped>
-  div{
+  .current-page{
     background: #262B30;
     height: 100vh;
   }
@@ -49,30 +71,69 @@ export default class ContactPage extends Vue {
     text-decoration: none;
     outline: 0;
   }
-  .contact-about{
+
+  .contact-form{
     position: relative;
-    height: auto;
-    bottom: 67vh;
-    left: 14vw;
+    width: 50vw;
+    height: 50vh;
+    bottom: 70vh;
+    z-index: 43;
     color: white;
-    font-size: 40px;
-    font-family: hero-new, sans-serif;
-    font-weight: 300;
-    font-style: normal;
-    border-bottom: none;
+  }
+  #name{
+    position: relative;
+    left: 14vw;
+    background-color: grey;
+    opacity: 0.5;
+    border-radius: 10px;
+    height: 30px;
+    width: 300px;
+    font-size: 30px;
   }
 
-  .the-styling{
-    text-decoration: none;
-    color: white;
+  #email{
+    position: relative;
+    left: 14vw;
+    background-color: grey;
+    opacity: 0.5;
+    border-radius: 10px;
+    height: 30px;
+    width: 300px;
+    font-size: 30px;
   }
+
+  #message{
+    position: relative;
+    top: 5vh;
+    left: 14vw;
+    background-color: grey;
+    opacity: 0.5;
+    border-radius: 10px;
+    height: 200px;
+    width: 300px;
+    font-size: 30px;
+  }
+
+  ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: white;
+    opacity: 1; /* Firefox */
+    align-self: start;
+  }
+
+  :-ms-input-placeholder { /* Internet Explorer 10-11 */
+    color: white;
+    align-self: start;
+  }
+
+  ::-ms-input-placeholder { /* Microsoft Edge */
+    color: white;
+    align-self: start;
+  }
+
 
   @media only screen and (max-width: 830px) {
     .contact-title{
       font-size: 50px;
-    }
-    .contact-about{
-      font-size: 21px;
     }
   }
 
@@ -80,11 +141,6 @@ export default class ContactPage extends Vue {
     .contact-title{
       font-size: 120px;
       bottom: 87vh;
-    }
-    .contact-about{
-      font-size: 40px;
-      bottom: 77vh;
-      line-height: 60px;
     }
   }
 </style>
